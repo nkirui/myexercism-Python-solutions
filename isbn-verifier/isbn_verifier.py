fb0c5b5ec21627f25f8d315b33d2a191b4819339
Nkirui <nathankirui5@gmail.com>
2019-04-04 00:19:05 +0300

    isbn solution

--- isbn-verifier/isbn_verifier.py
+++ isbn-verifier/isbn_verifier.py
@@ -1,2 +1,25 @@
 def verify(isbn):
-    pass
+    total = 0
+    multi = 10
+    isbk= [d for d in isbn if d.isalnum()]
+    if len(isbk) != 10:
+        return False
+    if len(isbk) < 10:
+        return False    
+    if isbk[-1].upper() == 'X':
+        isbk[-1]= str(10) 
+    try:
+        data = map(int,isbk)              
+        for i in data:
+            total += i*multi
+            multi -= 1
+        if total % 11 == 0:
+            return True
+        else:
+            return False
+    except :
+        return(False)
+
+print(verify("3-598-21507-x"))
+
+ 
