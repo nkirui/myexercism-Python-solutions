e95c7d10a5c2a5c05fe9a08c2cb6bbb23232f03e
Nkirui <nathankirui5@gmail.com>
2019-04-04 20:20:39 +0300

    hamming sol

--- hamming/hamming.py
+++ hamming/hamming.py
@@ -1,2 +1,24 @@
 def distance(strand_a, strand_b):
-    pass
+
+    distance = []
+    available = []
+    dataA = [a for a in strand_a] 
+    dataB = [b for b in strand_b]
+
+
+    if len(dataA) != len(dataB):
+            raise ValueError("The distance are not equal")   
+
+    for a, b in zip(dataA,dataB):             
+        
+        if a !=  b:
+            distance.append(a)
+
+        else:
+            available.append(a)
+
+    return len(distance)                  
+
+
+         
+
