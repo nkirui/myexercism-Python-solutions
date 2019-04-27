4a03dbf10eb3f09b7e8e9d298d969eab705b51e5
Nkirui <nathankirui5@gmail.com>
2019-04-27 10:01:27 +0300

    mentor approval chanages

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,11 +1,17 @@
 def is_isogram(string):  
-
-    data= "".join(sorted([i.upper() for i in string if i.isalpha()])).strip()
+    
+    # my solution
+    """data= "".join(sorted([i.upper() for i in string if i.isalpha()])).strip()
 
     data2= "".join(sorted({i.upper()for i in string if i.isalpha()})).strip()
 
-    if data != data2:
-        return False
-    else:
-        return True   
+    return data == data2   """
+
+    # Correction from the mentor (readability is key)                                                                               
+    filtered_text = list(filter(str.isalpha, string.upper())) 
+                                                            
+    sorted_text,isogram_text= filtered_text,set(filtered_text)    
+                                                            
+    return len(sorted_text) == len(isogram_text)
+
 
